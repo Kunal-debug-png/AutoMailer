4950b8be895d284eba73976d25713f9e56e1007e
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-12 11:20:07 +0530

    Initial commit

---
+++ MailSender/MailSender/src/test/java/com/kunal_debug_png/MailSender/MailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal_debug_png.MailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
